5746038f0b189dd686fd304741ffb83225e97c85
Brundashree-A <brundashree1807@gmail.com>
2024-10-21 20:12:20 +0530

    practice 21-10-24

--- pgm1.cpp
+++ pgm1.cpp
@@ -1,9 +1,32 @@
+// minimum number of an array
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(){
+int main()
+{
+    int i,j;
+    vector<int> a={2,5,3,1,0};
+    int n = a.size();
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+            swap(a[i],a[j]);
+            }
+        }
+        
+    }
+      cout<<"sorted array: "<<endl;
+    for(i=0;i<n;i++){
+        cout<<a[i]<<endl;
+    }
+    cout<<"minimum number is: "<<a[0]<<endl;
     
-    cout<<"hello world";
-
+   
 }
+
+
 
  
