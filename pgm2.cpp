720519c85dece8e96211dfeb94fa8232eab3f9fa
Brundashree-A <brundashree1807@gmail.com>
2024-11-08 12:50:05 +0530

    largest num of array

--- pgm2.cpp
+++ pgm2.cpp
@@ -1,46 +1,28 @@
 // largest number of an array "using bruteforce -sorting"
 
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// int sortArr(vector<int> & arr){
-//     sort(arr.begin(),arr.end());
-//     return arr[4];
-// }
-
-// int main(){
-//   vector<int>  arr1 = {2,5,4,6,1};
-//    vector<int> arr2 = {10,5,6,9,1};
-
-//    cout<<"maximum number is: "<< sortArr(arr1)<<endl ;
-//    cout<<"maximum number is: "<< sortArr(arr2) ;
-
-// }Time Complexity: O(N*log(N))  Space Complexity: O(n)
-
-
-// we can also slove by:
-#include<iostream>
-#include<vector> 
+#include<bits/stdc++.h>
 using namespace std;
-int largest(vector<int>& arr , int n){
-    for(int i=0; i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if( arr[i]>arr[j]){
-            swap(arr[i] , arr[j]);
-        }
-    }
-}
-return arr[n-1];
+
+int sortArr(vector<int> & arr){
+    sort(arr.begin(),arr.end());
+    return arr[4];
 }
 
 int main(){
-    vector<int> arr={9,8,4,7,10,1};
-    int n = arr.size();
-    cout<<"maximum number is: "<<largest(arr,n)<<endl;
+  vector<int>  arr1 = {2,5,4,6,1};
+   vector<int> arr2 = {10,5,6,9,1};
+
+   cout<<"maximum number is: "<< sortArr(arr1)<<endl ;
+   cout<<"maximum number is: "<< sortArr(arr2) ;
 
 }
+// Time Complexity: O(N*log(N))  Space Complexity: O(n)
+
+
+
 
 // using max variable
+
 // #include <bits/stdc++.h>
  
 // using namespace std;
@@ -68,3 +50,25 @@ int main(){
 // }
 
 // Time Complexity: O(N) Space Complexity: O(1)
+
+// we can also solve by: 
+// #include<iostream>
+// #include<vector> 
+// using namespace std;
+// int largest(vector<int>& arr , int n){
+//     for(int i=0; i<n-1;i++){
+//         for(int j=i+1;j<n;j++){
+//             if( arr[i]>arr[j]){
+//             swap(arr[i] , arr[j]);
+//         }
+//     }
+// }
+// return arr[n-1];
+// }
+
+// int main(){
+//     vector<int> arr={9,8,4,7,10,1};
+//     int n = arr.size();
+//     cout<<"maximum number is: "<<largest(arr,n)<<endl;
+
+// }
