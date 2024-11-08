084bf0f7a5dbabd7cf9892ab34da2151cad6bb13
Brundashree-A <brundashree1807@gmail.com>
2024-11-08 12:47:06 +0530

    second smallest and largest

--- pgm3.cpp
+++ pgm3.cpp
@@ -1,4 +1,61 @@
 // second smallest and second largest numbers
+// Best solution
+
+#include<bits/stdc++.h>
+using namespace std;
+int secondSmallest(int arr[],int n)
+{
+    if(n<2)
+        return -1;
+   int small = INT_MAX;
+   int second_small = INT_MAX;
+   int i;
+   for(i=0;i<n;i++){
+    if(arr[i]<small){
+        second_small=small;
+        small = arr[i];
+    }
+    else if(arr[i]<second_small && arr[i]!=small)
+    {
+        second_small=arr[i]; 
+    }
+    }
+   return second_small;
+}
+
+int secondLargest(int arr[],int n){
+    if(n<2)
+    return -1;
+    int large = INT_MIN;
+    int second_large =INT_MIN;
+    int i;
+    for(i=0;i<n;i++){
+        if(arr[i]>large){
+            second_large= large;
+            large = arr[i];
+        }
+        else if(arr[i]>second_large && arr[i]!=large){
+            second_large=arr[i];
+        }
+    }
+    return second_large;
+}
+
+
+
+int main() {
+    int arr[]={1,2,4,7,7,5};  
+    int n=sizeof(arr)/sizeof(arr[0]);
+        int sS=secondSmallest(arr,n);
+        int sL=secondLargest(arr,n);
+    cout<<"Second smallest is "<<sS<<endl;
+    cout<<"Second largest is "<<sL<<endl;
+    return 0;
+}
+
+// normal method
+
+
 // #include<iostream>
 // #include <bits/stdc++.h>
 // using namespace std;
@@ -105,58 +162,4 @@
 //     return 0;
 // }
 
-
-// Best solution
-
-#include<bits/stdc++.h>
-using namespace std;
-int secondSmallest(int arr[],int n)
-{
-    if(n<2)
-        return -1;
-   int small = INT_MAX;
-   int second_small = INT_MAX;
-   int i;
-   for(i=0;i<n;i++){
-    if(arr[i]<small){
-        second_small=small;
-        small = arr[i];
-    }
-    else if(arr[i]<second_small && arr[i]!=small)
-    {
-        second_small=arr[i]; 
-    }
-    }
-   return second_small;
-}
-
-int secondLargest(int arr[],int n){
-    if(n<2)
-    return -1;
-    int large = INT_MIN;
-    int second_large =INT_MIN;
-    int i;
-    for(i=0;i<n;i++){
-        if(arr[i]>large){
-            second_large= large;
-            large = arr[i];
-        }
-        else if(arr[i]>second_large && arr[i]!=large){
-            second_large=arr[i];
-        }
-    }
-    return second_large;
-}
-
-
-
-int main() {
-    int arr[]={1,2,4,7,7,5};  
-    int n=sizeof(arr)/sizeof(arr[0]);
-        int sS=secondSmallest(arr,n);
-        int sL=secondLargest(arr,n);
-    cout<<"Second smallest is "<<sS<<endl;
-    cout<<"Second largest is "<<sL<<endl;
-    return 0;
-}
 
