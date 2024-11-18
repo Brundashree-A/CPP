03f8b3c3417845739c297513949c931080f79fcb
Brundashree-A <brundashree1807@gmail.com>
2024-11-18 11:38:14 +0530

    removeduplicateinsortedarray

--- pgm11.cpp
+++ pgm11.cpp
@@ -1,32 +1,60 @@
 // Remove Duplicates in-place from Sorted Array
-// removing the duplicate or repeated numbers from an array
+// optimal solution - using two poniters
+
 #include<bits/stdc++.h>
+
 using namespace std;
-
-int duplicate(vector<int> &arr, int n){
-    for(int i=0;i<n;i++){
-        for(int j=1;j<n;j++){
-            if(arr[i]!=arr[j]){
-              i++;
-              arr[i]==arr[j];
-            }
-            else{
-                cout<<arr[j];
-            }
-        }
-
+int removeDuplicates(int arr[], int n)
+{
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
     }
-    
+  }
+  return i + 1;
+}
+int main() {
+  int arr[] = {1,1,2,2,2,3,3};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int k = removeDuplicates(arr, n);
+  cout << "The array after removing duplicate elements is " << endl;
+  for (int i = 0; i < k; i++) {
+    cout << arr[i] << " ";
+  }
 }
 
-int main(){
-    int n;
-    cout<<"enter the n value: ";
-    cin>>n;
-    vector<int> arr(n);
-    cout<<"enter the array elements:";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    duplicate(arr,n);
-}
+// time complexity = O(n) space complexity = O(1)
+
+
+// // using brute force technique:-
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int removeDuplicates(int arr[], int n) {
+//   set < int > set;
+//   for (int i = 0; i < n; i++) {
+//     set.insert(arr[i]);
+//   }
+//   int k = set.size();
+//   int j = 0;
+//   for (int x: set) {
+//     arr[j++] = x;
+//   }
+//   return k;
+// }
+// int main() {
+//   int arr[] = {1,1,2,2,2,3,3};
+//   int n = sizeof(arr)/sizeof(arr[0]);
+//   int k = removeDuplicates(arr, n);
+//   cout << "The array after removing duplicate elements is " << endl;
+//   for (int i = 0; i < k; i++) {
+//     cout << arr[i] << " ";
+//   }
+// }
+
+// Time complexity: O(n*log(n))+O(n)
+// Space Complexity: O(n) 
+
