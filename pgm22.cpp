0831d17240510c16e931314fddf854d6cf9b45f2
Brundashree-A <brundashree1807@gmail.com>
2024-12-20 19:06:50 +0530

    search an element

--- pgm22.cpp
+++ pgm22.cpp
@@ -26,3 +26,32 @@ int main(){
 
 // time complexity : O(n)
 // space complexity : O(1)
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+// 	int n = 6;
+// 	int arr[n] = {6, 7, 9, 5, 3, 10};
+// 	int k = 10;
+// 	int low = 0, high = n - 1;
+// 	int ans = -1;
+// 	while (low <= high) {
+// 		int mid = (low + high) / 2;
+// 		if (arr[mid] > k) {
+// 			high = mid - 1;
+// 		}
+// 		else if (arr[mid] < k) {
+// 			low = mid + 1;
+// 		}
+// 		else {
+// 			ans = mid;
+// 			break;
+// 		}
+// 	}
+// 	cout << "The element is present in "<<ans<<" index";
+// }
+
+// Time Complexity: O(logn).
+
+// Space Complexity: O(1)
---
+++ pgm23.cpp
@@ -0,0 +1,15 @@
+// Check if array is subset of another array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int subset(vector<int> arr[],int n){
+
+}
+
+void main(){
+    int n;
+    vector<int> arr[n];
+
+    
+}
